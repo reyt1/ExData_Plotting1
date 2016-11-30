443b707968195867f5915d0199b6c7b1efb8412a
reyt1 <reynaldo.ns@gmail.com>
2016-11-30 12:31:56 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,12 +2,14 @@
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 DataSubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-Subsettime <- as.Date(subSetData$Date, format = '%d/%m/%Y')
-DateTime <- as.POSIXct(paste(Subsettime, subSetData$Time))
+DataSubset$Date<- as.Date(DataSubset$Date, format = '%d/%m/%Y')
+DataSubset$DataTime <- as.POSIXct(paste(DataSubset$Date, DataSubset$Time))
 if(!file.exists("Graphics")) dir.create("Graphics")
 png(filename = './Graphics/plot2.png', width = 480, height = 480, units='px')
 # plot figure
 Sys.setlocale(category = "LC_ALL", locale = "english")
-plot(DateTime, as.numeric(subSetData$Global_active_power), xlab = "", ylab = "Global Active Power (kilowatt)", type = "l")
+#plot(DateTime, as.numeric(DataSubset$Global_active_power), xlab = "", ylab = "Global Active Power (kilowatt)", type = "l")
+plot(DataSubset$DateTime, as.numeric(DataSubset$Global_active_power), xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+  #Close device
 dev.off()
 
