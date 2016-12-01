0e9f336086fdc705c40ef5371efcf638e68d48f7
reyt1 <reynaldo.ns@gmail.com>
2016-12-01 07:44:47 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,15 +1,13 @@
-# read data into R
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-DataSubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-DataSubset$Date<- as.Date(DataSubset$Date, format = '%d/%m/%Y')
-DataSubset$DataTime <- as.POSIXct(paste(DataSubset$Date, DataSubset$Time))
-if(!file.exists("Graphics")) dir.create("Graphics")
-png(filename = './Graphics/plot2.png', width = 480, height = 480, units='px')
+# Plot 2
+# convert data and time to specific format
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot2.png', width = 480, height = 480, units='px')
 # plot figure
 Sys.setlocale(category = "LC_ALL", locale = "english")
-#plot(DateTime, as.numeric(DataSubset$Global_active_power), xlab = "", ylab = "Global Active Power (kilowatt)", type = "l")
-plot(DataSubset$DateTime, as.numeric(DataSubset$Global_active_power), xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
-  #Close device
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+# close device
 dev.off()
 
