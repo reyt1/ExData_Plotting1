993d78964b3aa2e00cabbf4bd8158429dfa2a823
reyt1 <reynaldo.ns@gmail.com>
2016-12-01 08:30:51 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,11 +3,10 @@
 data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
 data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
 # open device
-if(!file.exists('figures')) dir.create('figures')
-png(filename = './figures/plot2.png', width = 480, height = 480, units='px')
+if(!file.exists("Graphics")) dir.create("Graphics")
+png(filename = './Graphics/plot2.png', width = 480, height = 480, units="px")
 # plot figure
 Sys.setlocale(category = "LC_ALL", locale = "english")
-plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(data$DateTime, data$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatt)", type = "l")
 # close device
 dev.off()
-
