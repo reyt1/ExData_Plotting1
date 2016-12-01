17cfa1c0b26e0e08fde2d97083ee577745a023cf
reyt1 <reynaldo.ns@gmail.com>
2016-12-01 08:49:58 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,11 +4,15 @@ getwd()
 "C:/Users/Reynaldo/Documents"
 
 # download and unzip data
-if(!file.exists('data')) dir.create('data')
+if(!file.exists("data")) dir.create("data")
 fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-download.file(fileUrl, destfile = './data/PowerConsumption.zip')
-unzip('./data/household_power_consumption.zip', exdir = './data')
+download.file(fileUrl, destfile = "./data/PowerConsumption.zip")
+unzip("./data/household_power_consumption.zip", exdir = "./data")
 
+# read data into R
+files <- file("./data/household_power_consumption.txt")
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ";", col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = "?")
+  
 # Plot 1
 # open device
 if(!file.exists("Graphics")) dir.create("Graphics")
